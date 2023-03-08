a1784226c9552132b268a2321ec44ee9e755da0f
Vini Antunes <vini.antunes2705@gmail.com>
2023-03-08 17:59:09 -0300

    Add files about crawler

---
+++ crawler/steps/upload_to_BigQuery.py
@@ -0,0 +1,35 @@
+import json
+from creds.credentials import credentials
+
+def upload_to_BigQuery(data):
+    '''
+    Upload data from a pandas DataFrame. An error is thrown if there is a connection problem.
+    
+    :param data: pandas DataFrame
+    
+    :return: no return.
+    '''
+    # Prepare the upload
+    with open('crawler\infos\BigQuery_infos.json') as file:
+        infos_BQ = json.load(file)
+    file.close()
+    
+    project_id = infos_BQ['project_id']
+    dataset_id = infos_BQ['dataset_id']
+    table_id = infos_BQ['table_id']
+    if_exists = infos_BQ['if_exists']
+    table_schema = infos_BQ['table_schema']
+
+    destination_table = f'{dataset_id}.{table_id}'
+
+    try:
+        data.to_gbq(
+            credentials=credentials(),
+            destination_table=destination_table,
+            if_exists=if_exists,
+            project_id=project_id,
+            table_schema=table_schema
+        )
+        print(f'Successfully uploaded to "{project_id}.{destination_table}" !!!')
+    except:
+        ConnectionError(f'Error when trying to upload data to BigQuery')
